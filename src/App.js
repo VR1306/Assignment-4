fe2b1228c281f6d89d8b75135dc3cc48f1534b1f
VR1306 <vijay13.doodleblue@gmail.com>
2022-05-12 13:32:21 +0530

    Assignment4

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Fourth from './Fourth'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Fourth/>
+      </div>
+    )
+  }
+}
+
+export default App
